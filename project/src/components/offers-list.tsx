12f311b33b63344d0106a1c3c9e75083680b8ff2
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-03-09 16:33:16 +0300

    active offer state belongs to OffersList component

--- project/src/components/offers-list.tsx
+++ project/src/components/offers-list.tsx
@@ -5,10 +5,11 @@ import OfferCard from './offer-card';
 
 function OffersList(props: Props): JSX.Element {
   const { offers } = props;
-  const [active, setActive] = useState(null as number|null);
+  const [, setActive] = useState(null as number|null);
+
   return (
     <div className="cities__places-list places__list tabs__content">
-      {offers.map((offer) => (<OfferCard key={offer.id} id={offer.id} />))}
+      {offers.map((offer) => (<OfferCard key={offer.id} id={offer.id} onActivate={()=>setActive(offer.id)} onDeactivate={()=>setActive(null)} />))}
     </div>
   );
 }
