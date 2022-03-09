bb82a1628bd97f0df748d13c49366b3ffc4d4c1c
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-03-09 16:15:07 +0300

    extracts goods - property-inside component

---
+++ project/src/components/room/property-inside-item.tsx
@@ -0,0 +1,9 @@
+interface PropertyInsideItemProps{
+  item: string;
+}
+export function PropertyInsideItem(props: PropertyInsideItemProps){
+  const {item} = props;
+  return (
+    <li className="property__inside-item">{item}</li>
+  );
+}
