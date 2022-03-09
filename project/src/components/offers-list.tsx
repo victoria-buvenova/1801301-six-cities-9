53c384d6fe174d23bbfd4f90b341b22ab406e7c1
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-03-09 16:21:46 +0300

    allocate state - useState

--- project/src/components/offers-list.tsx
+++ project/src/components/offers-list.tsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import { Props } from './app/app-props';
 import OfferCard from './offer-card';
 
 
 function OffersList(props: Props): JSX.Element {
   const { offers } = props;
+  const [active, setActive] = useState(null as number|null);
   return (
     <div className="cities__places-list places__list tabs__content">
       {offers.map((offer) => (<OfferCard key={offer.id} id={offer.id} />))}
