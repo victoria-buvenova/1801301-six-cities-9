896419d522d0e413e5444432002b8c8f29600f64
Victoria Buvenova <buvenova@gmail.com>
2022-03-15 23:43:23 +1300

    added correct location coordinates

--- project/src/mocks/offers.ts
+++ project/src/mocks/offers.ts
@@ -26,8 +26,8 @@ export const offers = [
     'isFavorite': false,
     'isPremium': false,
     'location': {
-      'latitude': 52.35514938496378,
-      'longitude': 4.673877537499948,
+      'latitude': 52.3909553943508,
+      'longitude': 4.85309666406198,
       'zoom': 8,
     },
     'maxAdults': 4,
@@ -64,8 +64,8 @@ export const offers = [
     'isFavorite': true,
     'isPremium': false,
     'location': {
-      'latitude': 52.35514938496378,
-      'longitude': 4.673877537499948,
+      'latitude': 52.369553943508,
+      'longitude': 4.85309666406198,
       'zoom': 8,
     },
     'maxAdults': 4,
@@ -102,8 +102,8 @@ export const offers = [
     'isFavorite': true,
     'isPremium': false,
     'location': {
-      'latitude': 52.35514938496378,
-      'longitude': 4.673877537499948,
+      'latitude': 52.3909553943508,
+      'longitude': 4.929309666406198,
       'zoom': 8,
     },
     'maxAdults': 4,
@@ -140,8 +140,8 @@ export const offers = [
     'isFavorite': true,
     'isPremium': true,
     'location': {
-      'latitude': 52.35514938496378,
-      'longitude': 4.673877537499948,
+      'latitude': 52.3809553943508,
+      'longitude': 4.939309666406198,
       'zoom': 8,
     },
     'maxAdults': 4,
