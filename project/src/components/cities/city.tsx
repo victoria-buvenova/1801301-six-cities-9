cc2b8f38ced69bbb55788e32cf9b0bb863333475
Victoria Buvenova <buvenova@gmail.com>
2022-03-27 00:26:23 +1300

    added click handler on city

--- project/src/components/cities/city.tsx
+++ project/src/components/cities/city.tsx
@@ -1,11 +1,21 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { getCurrentCity } from '../../selectors/get-current-city';
+import { cityChangeAction } from '../../store/action';
+
 type CityProps = { cityName: string }
 
 function City(props: CityProps) {
-  const { cityName: city } = props;
+  const { cityName } = props;
+  const currentCityName = useSelector(getCurrentCity);
+  const dispatch = useDispatch();
+  const handleCityClick = (selectedCityName: string) => {
+    dispatch(cityChangeAction(selectedCityName));
+  };
+
   return (
     <li className="locations__item">
-      <a className={`locations__item-link tabs__item ${city === 'Paris' ? 'tabs__item--active' : ''}`} href="#work-in-progress">
-        <span>{city}</span>
+      <a onClick={() => handleCityClick(cityName)} className={`locations__item-link tabs__item ${cityName === currentCityName ? 'tabs__item--active' : ''}`} href="#work-in-progress">
+        <span>{cityName}</span>
       </a>
     </li>
   );
