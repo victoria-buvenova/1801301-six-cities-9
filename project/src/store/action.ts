5df73772825499a85765880fce0a70bc12af486c
Victoria Buvenova <buvenova@gmail.com>
2022-03-29 00:44:23 +1300

    added action, selectors, sorting

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -3,6 +3,7 @@ import { createAction } from '@reduxjs/toolkit';
 export const Action = {
   CITY_CHANGE: 'CITY_CHANGE',
   OFFERS_LIST_FILL: 'OFFERS_LIST_FILL',
+  SORT_BY_CHANGE_ACTION: 'SORT_BY_CHANGE_ACTION',
 };
 
 
@@ -11,5 +12,9 @@ export const cityChangeAction = createAction(Action.CITY_CHANGE, (value) => ({
 }));
 
 export const offersListFillAction = createAction(Action.OFFERS_LIST_FILL, (value) => ({
+  payload: value,
+}));
+
+export const sortByChangeAction = createAction(Action.SORT_BY_CHANGE_ACTION, (value) => ({
   payload: value,
 }));
